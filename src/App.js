7fc36eaf25c008b0552491f2ab9c3b201085049d
Justin Diaz <justin.s.diaz@gmail.com>
2021-09-08 11:23:02 -0400

    added more cars to the carData object

--- src/App.js
+++ src/App.js
@@ -40,7 +40,27 @@ const carData = [{
     location:"993 Fiscal St Boulder, CO 93391",
     price:"37,490",
     liked: false   
-  }]
+    },{
+    name:"2017 Toyota Highlander",
+    location:"1944 Scrimba Ln Youngstown, OH 44481",
+    price:"17,400",
+    liked: false  
+    },{
+    name:"2012 Toyota Tundra 4x4",
+    location:"301 Fantasy Dr Beverly Hills, CA 90210",
+    price:"23,900",
+    liked: false   
+    },{
+    name:"2004 Dodge Ram 4x4",
+    location:"9104 Falsety Ave AnonyVille, FL 40440",
+    price:"7,998",
+    liked: false   
+    },{
+    name:"2021 Tesla Model 3",
+    location:"993 Fiscal St Boulder, CO 93391",
+    price:"37,490",
+    liked: false   
+    }]
 
 function App() {
 
